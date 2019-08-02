96f1fd77810f29552c76dcf8486cfa52ef251d56
dzzzzzzzzzzze <47446112+dzzzzzzzzzzze@users.noreply.github.com>
2019-08-02 23:22:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -79,24 +79,7 @@ if (message.content === prefix+'spam') {
         }
       }
 });
-    client.on('message', message => {
-        var prefix = "#";
-        if (message.author.bot) return;
-        if (!message.content.startsWith(prefix)) return;
-      
-        let command = message.content.split(" ")[0];
-        command = command.slice(prefix.length);
-      
-      
-      let args = message.content.split(" ").slice(1);
-      let x = args.join(" ")
-        if(message.content.startsWith(prefix + 'say')) {
-            message.channel.send(''+x);
-                message.delete(999)
-        }
-        
-       
-      });
+
 client.on('message', message => {
   if (message.author.bot) return;
   if (!message.content.startsWith(prefix)) return;
